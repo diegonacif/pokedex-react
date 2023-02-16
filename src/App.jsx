10ce59b8685bc303a05c45b8bbafd62199483f3f
diegonacif <diegoreisnacif@gmail.com>
2023-02-15 22:13:10 -0300

    feat: api getting informations to card

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,29 @@ import { Card } from './components/Card/Card';
 
 
 export const App = () => {
+  const [pokemon, setPokemon] = useState(null);
+  const official = "official-artwork";
+
+  useEffect(() => {
+    api.get("nidoqueen").then(({data}) => {
+      setPokemon(data);
+    })
+    .catch(() => {
+      console.log("error")
+    });
+  }, []);
+  console.log(pokemon?.sprites.other.official)
   
   return (
     <div className="app-body">
       <main>
         <div className="main-content">
-          <Card />
+          <Card 
+            id={pokemon?.id}
+            name={pokemon?.name}
+            types={pokemon?.types}
+            // img={pokemon?.other}
+          />
         </div>
       </main>
     </div>
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,18 +1,23 @@
 import pokeImg from '../../assets/001.png';
 
-export const Card = () => {
+export const Card = ({ id, name, types }) => {
   return (
     <div className="card-container">
       <div className="img-wrapper">
-        <img src={pokeImg} alt="" />
+        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`} alt="" />
       </div>
       <div className="card-info">
-        <p>Nº 0001</p>
-        <h3>Bulbasaur</h3>
+        <p>Nº {id}</p>
+        <h3>{name}</h3>
       </div>
       <div className="type-wrapper">
-        <span className="type-pill">Grass</span>
-        <span className="type-pill">Poison</span>
+        {/* <span className="type-pill">Grass</span>
+        <span className="type-pill">Poison</span> */}
+        {
+          types?.map((data) =>
+            <span className="type-pill">{data.type.name}</span>
+          )
+        }
       </div>
     </div>
   )
