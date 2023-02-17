10c1bbf94f687abf0607b517835ec5490b9b8f44
diegonacif <diegoreisnacif@gmail.com>
2023-02-17 18:49:16 -0300

    att

--- src/App.jsx
+++ src/App.jsx
@@ -9,54 +9,64 @@ export const App = () => {
   const [pokemon, setPokemon] = useState(null);
   const [pokemonNames, setPokemonNames] = useState(null);
   const [pokemonSearchInput, setPokemonSearchInput] = useState("");
+  const [pokemonShownList, setPokemonShownList] = useState(null);
 
-   // Get Pokemon names list
+  // Get Pokemon names list
   useEffect(() => {
     //?limit=1279
-    api.get("?limit=1279").then(({data}) => {
+    api.get("?limit=1279").then(({ data }) => {
       setPokemonNames(
         data?.results.map(data => data.name)
       );
     })
-    .catch(() => {
-      console.log("error on getting names")
-    });
+      .catch(() => {
+        console.log("error on getting names")
+      });
   }, []);
 
-  // Get Pokemon By Name
+  // Get all Pokemon
   useEffect(() => {
-    api.get("bulbasaur").then(({data}) => {
+    api.get("?limit=1279").then(({ data }) => {
       setPokemon(data);
     })
-    .catch(() => {
-      console.log("error on get pokemon data")
-    });
-  }, []);
+      .catch(() => {
+        console.log("error on get pokemon data")
+      });
+  }, [pokemonSearchInput]);
 
   function handlePokemonSearch(e) {
     setPokemonSearchInput(e.target.value);
   }
 
-  function matchName(name) {
-    if (name === pokemonSearchInput) {
-      return name;
-    }
-  }
+  // function matchName(name) {
+  //   if (name === pokemonSearchInput) {
+  //     return name;
+  //   }
+  // }
 
-  const searchResult = pokemonNames?.filter(name => name.includes(pokemonSearchInput))
+  // const searchResult = pokemonNames?.filter(name => name.includes(pokemonSearchInput))
 
-  console.log(searchResult)
-    
+  console.log(pokemon?.results.map(data => data.name.indexOf("ivysaur")))
+
   return (
     <div className="app-body">
       <main>
         <div className="main-content">
           <input type="text" onChange={(e) => handlePokemonSearch(e)} />
-          <Card 
+          {
+            pokemon?.results.map(pokemon => {
+              <Card
+                // id={pokemon?.id}
+                name={pokemon?.name}
+                types={pokemon?.types}
+              />
+            })
+          }
+          {/* <Card 
             id={pokemon?.id}
             name={pokemon?.name}
             types={pokemon?.types}
-          />
+          /> */}
         </div>
       </main>
     </div>
